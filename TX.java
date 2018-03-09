ff177721d03c0702ecd2cd004df1da36ace59a30
LammingerL <32764659+LammingerL@users.noreply.github.com>
2018-03-09 15:45:34 +0100

    Add files via upload

---
+++ TX.java
@@ -0,0 +1,40 @@
+package TX_j;
+
+import java.io.IOException;
+import java.net.*;
+import java.util.*;
+
+
+//Parameter sollen irgendwo eingegeben werden können
+//byte->string bzw umgekehrt für C
+//Messen?
+class TX
+{
+  public static void main( String[] args ) throws IOException,
+                  InterruptedException
+  {
+    InetAddress ia = InetAddress.getByName( "127.0.0.1" );
+    
+    int count = 0;
+    int anz_pakete= 100;
+    
+    while ( count<anz_pakete )
+    {
+      String s = "00101";
+      byte[] raw = s.getBytes();
+     
+
+      DatagramPacket packet = new DatagramPacket( raw, raw.length, ia, 4711 );
+
+      DatagramSocket dSocket = new DatagramSocket();
+
+      dSocket.send( packet );
+
+      System.out.println( "Paket gesendet" );
+      count++;
+
+      
+    }
+  }
+}
+
