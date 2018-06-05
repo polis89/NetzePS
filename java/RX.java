cad52558b8583d99dcea36e88813b037f8c1c43b
LammingerL <32764659+LammingerL@users.noreply.github.com>
2018-06-05 23:43:38 +0200

    Add files via upload

--- java/RX.java
+++ java/RX.java
@@ -1,144 +1,286 @@
+
+
 /**
+
  * RX
+
  * Usage: RX <portTX> <portRX>
+
  *
+
  * @author Dmitrii Polianskii, Lukas Lamminger
+
  */
 
+
+
 import java.io.*;
+
 import java.net.*;
+
 import java.util.*;
+
 import java.util.zip.CRC32;
 
+
+
 public class RX{
+
 	private static final int MAX_BUF_SIZE = 8192;
-  private static int portAck = 4700; //Default port to send acks
-  private static int port = 4711; //Default port
+
+	private static int portRX = 4711; //Default port to send acks
+	
+	private static int portTX = 4700; //Default port to send acks
+	
 	private static byte[] buf = new byte[1_000_000]; //Default buffer
+
 	private static HashMap<Integer, ArrayList<Byte>> recievedData = new HashMap<>();
+
 	private static ArrayList<Boolean> acks = new ArrayList<>();
+
 	private static int packets_amount = -1; //-1 for unbestimmt
+
 	private static int packet_payload = -1; //-1 for unbestimmt
 
+
+
   private static InetAddress ia;
+
   private static CRC32 crc = new CRC32();
+
+
 
 	public static void main(String[] args) throws IOException, InterruptedException{
 
-		ia = InetAddress.getByName( "127.0.0.1" );
+
+
+	
+
+
 
 		if (args.length > 0){
-			portAck = Integer.parseInt(args[0]);
-		}
-		if (args.length > 1){
-			port = Integer.parseInt(args[1]);
+
+			portRX = Integer.parseInt(args[0]);
+
 		}
 
-		DatagramSocket socket = new DatagramSocket(port);
+
+
+		
+
+		DatagramSocket socket = new DatagramSocket(portRX);
+
 		DatagramSocket dSocketAck = new DatagramSocket();
+
 		// int packetSize = s.getBytes().length + 4;
+
 		// byte[] buf = new byte[packetSize];
 
+
+
 		long count = 0;
+
 		long packet_recieved = 0;
 
+
+
 		//Listen Loop
+
 		while(packets_amount != packet_recieved){
+
 			DatagramPacket packet = new DatagramPacket(buf, buf.length);
+
 			socket.receive(packet);
-			System.out.println("=== Recieve ===");
+
+			//System.out.println("=== Recieve ===");
+
+			ia = packet.getAddress();
 
 			DataInputStream dis = new DataInputStream(new ByteArrayInputStream(packet.getData())); //Make input stream from datagramm
+
 			int sequenzeNumInt = dis.readInt();
 
+
+
 			if(sequenzeNumInt < 0){ //with 2-complement first bit makes int negative
+
 				System.out.println("Last packet");
+
       	sequenzeNumInt &= (0b0111111111111111); //Remove first bit
+
       	packets_amount = sequenzeNumInt + 1;
+
 			}else if (packet_payload < 0){
+
 				// packet_payload unbestimmt
+
 				packet_payload = packet.getLength() - 4;
+
 			}
-			System.out.println("Sequenz Nummer: " + sequenzeNumInt);
+
+		//	System.out.println("Sequenz Nummer: " + sequenzeNumInt);
+
 			if(acks.size() <= sequenzeNumInt){
+
 				int size_ack = acks.size();
+
 				for(int i = 0; i < sequenzeNumInt - size_ack + 1;i++)
+
 					acks.add(false);
+
 			}
+
 			if(!acks.get(sequenzeNumInt)){
+
 				acks.set(sequenzeNumInt, true);
+
 				packet_recieved++;
+
 				ArrayList<Byte> dataFromDatagram = new ArrayList<>();
+
 				for(int i = 0; i < packet.getLength() - 4; i++){
+
 					dataFromDatagram.add(dis.readByte());
+
 				}
+
 				recievedData.put(sequenzeNumInt, dataFromDatagram);
+
 			}
+
 			byte[] axkData = convertIntTo4Bytes(sequenzeNumInt);
-    	DatagramPacket ack_packet = new DatagramPacket( axkData, axkData.length, ia, portAck ); 
+			
+			
+
+    	DatagramPacket ack_packet = new DatagramPacket( axkData, axkData.length, ia, portTX); 
+
     	dSocketAck.send( ack_packet );
+    	
+    	//System.out.println("ack send");
 
 		}
 
 		System.out.println("packet_payload: " + packet_payload);
+
 		System.out.println("recievedData size (in packets): " + recievedData.size());
 
+
+
 		assembleFile();
+
 		crc32check();
+
 	}
+
+
 
   private static byte[] convertIntTo4Bytes(long value){
 
+
+
     byte [] data = new byte[4];
+
     data[3] = (byte) value;
+
     data[2] = (byte) (value >>> 8);
+
     data[1] = (byte) (value >>> 16);
+
     data[0] = (byte) (value >>> 24);
+
+
 
     return data;
 
+
+
   }
+
+
 
   private static void assembleFile() throws IOException{
+
 		System.out.println("assembleFile");
+
   	FileOutputStream fos = new FileOutputStream("output.jpg");
+
   	for(int i = 0; i < packets_amount-1; i++){
+
   		ArrayList<Byte> packet = recievedData.get(i);
+
   		for(int j = 0; j < packet_payload; j++){
+
     		fos.write(packet.get(j));
+
     		crc.update(packet.get(j));
+
   		}
+
   	}
+
   	ArrayList<Byte> lastPackage = recievedData.get(packets_amount-1);
+
 		System.out.println("last packet size: " + lastPackage.size());
 
+
+
   	for(int i = 0; i < lastPackage.size()-4; i++){
+
     	fos.write(lastPackage.get(i));
+
   		crc.update(lastPackage.get(i));
+
   	}
+
     fos.close();
+
   }
+
   private static void crc32check(){
+
 		//From Packets
+
 		ArrayList<Byte> lastPackage = recievedData.get(packets_amount-1);
+
 		int lastPackage_s = lastPackage.size();
+
 		byte[] crcvalue = new byte[4];
+
     // System.out.println("Last:   " + lastPackage.toString());
 
+
+
 		for(int i = 0;i<4;i++)		{
+
 			crcvalue[i] = lastPackage.get(lastPackage_s - 4 + i);
+
 		}
-    System.out.println("CRC32 calcuated:   " + crc.getValue());
-    System.out.println("CRC32 received: " + Integer.toUnsignedString(fromByteArray(crcvalue)));   
-    System.out.println("CRC32 received: " + (crcvalue[0] & 0xFF) + " "+ (crcvalue[1] & 0xFF) + " " + (crcvalue[2] & 0xFF) + " " + (crcvalue[3] & 0xFF));   
-    if(crc.getValue() == fromByteArray(crcvalue)){
+		
+
+    System.out.println("CRC32 calcuated: " + crc.getValue());
+
+    System.out.println("CRC32 received:  " + Integer.toUnsignedString(fromByteArray(crcvalue)));   
+
+    //System.out.println("CRC32 received: " + (crcvalue[0] & 0xFF) + " "+ (crcvalue[1] & 0xFF) + " " + (crcvalue[2] & 0xFF) + " " + (crcvalue[3] & 0xFF));   
+
+    if(crc.getValue() == Long.parseLong(Integer.toUnsignedString(fromByteArray(crcvalue)))){
+
     	System.out.println("CRC32 is correct");
+
     }else{
+
     	System.out.println("CRC32 is NOT correct");
+
     }
+
   }
+
 	 private static int fromByteArray(byte[] bytes)
+
 	 {
+
 	     return (bytes[0] & 0xFF) << 24 | (bytes[1] & 0xFF) << 16 | (bytes[2] & 0xFF) << 8 | (bytes[3] & 0xFF);
+
 	 }
+
 }
--- java/TX.java
+++ java/TX.java
@@ -1,247 +1,505 @@
+
 /**
+
  *
+
  * Usage: java TX <portTX> <portRX> <packet_size> <packet_block_size> <send_delay> <file_name>
+
  * Default: TX 4700 4711 1000 100 0 to_send.jpg
+
  *
+
  * @author Dmitrii Polianskii, Lukas Lamminger
+
  */
 
 
+/**
+
+ *
+
+ * Usage: java TX <portTX> <portRX> <packet_size> <packet_block_size> <send_delay> <file_name>
+
+ * Default: TX 4700 4711 1000 100 0 to_send.jpg
+
+ *
+
+ * @author Dmitrii Polianskii, Lukas Lamminger
+
+ */
+
+
+
+
+
 import java.io.ByteArrayInputStream;
+
 import java.io.ByteArrayOutputStream;
+
 import java.io.DataInputStream;
+
 import java.io.File;
+
 import java.io.FileInputStream;
+
+
 
 import java.io.IOException;
 
+
+
 import java.net.*;
 
+
+
 import java.util.*;
+
 import java.util.Collections;
 
 
+
+
+
 import java.util.concurrent.*;
+
 import java.util.zip.CRC32;
 
+
+
 class TX
+
 {
 
+
+
   //Default params
-  private static int ack_port = 4700;
-  private static int port = 4711;
+
+  private static int portTX = 4700;
+
+  private static int portRX = 4711;
+
   private static int msgsize = 1000;
-  private static int packet_block_size = 100;
-  private static int delay = 200;
-  private static String fileName = "to_send2.jpg";
+
+  private static int delay = 500;
+  
+  private static String ip_adress = "127.0.0.1";
+
+  private static String fileName = "BaseImage.jpg";
+
+
 
   // Acks
+
   private static boolean[] ack_seq;
 
+
+
   static ArrayList<DatagramPacket> packets;
+
   private static byte[] contentToTransmit;
+
   private static int anz_pakete;
+
   private static DatagramSocket dSocket, dSocketAck;
+
   private static InetAddress ia;
 
 
+
+
+
   public static void main( String[] args ) throws IOException, InterruptedException {
+
   
+
     if (args.length > 0){
-      ack_port = Integer.parseInt(args[0]);
+
+      portTX = Integer.parseInt(args[0]);
+
     }
+
   
+
     if (args.length > 1){
-      port = Integer.parseInt(args[1]);
+
+      portRX = Integer.parseInt(args[1]);
+
     }
+
   
+
     if (args.length > 2){
+
       msgsize = Integer.parseInt(args[2]);
+
     }
+
     
+
+
     if (args.length > 3){
-      packet_block_size = Integer.parseInt(args[3]);
+
+      delay = Integer.parseInt(args[3]);
+
     }
-    
+
     if (args.length > 4){
-      delay = Integer.parseInt(args[4]);
-    }
+
+        ip_adress = args[4];
+
+      }
+
 
     if (args.length > 5){
+
       fileName = args[5];
+
     }
 
+
+
     packets = new ArrayList<DatagramPacket>();
+
     dSocket = new DatagramSocket();
-    dSocketAck = new DatagramSocket(ack_port);
-    ia = InetAddress.getByName( "127.0.0.1" );
+
+    dSocketAck = new DatagramSocket(portTX);
+
+    ia = InetAddress.getByName(ip_adress);
+
    
+
     byte[] fileContent = readFile();
+
     System.out.println("File size = " + fileContent.length);
 
+
+
     CRC32 crc = new CRC32();
+
     crc.update(fileContent);
+
     byte[] checksum = convertIntTo4Bytes(crc.getValue());
 
+
+
     System.out.print("CRC32: " + Arrays.toString(checksum));
+
     System.out.println(" / " + crc.getValue());
 
+
+
     contentToTransmit = new byte[fileContent.length + 4];
+
     System.arraycopy(fileContent, 0, contentToTransmit, 0, fileContent.length);
+
     System.arraycopy(checksum, 0, contentToTransmit, fileContent.length, 4);
 
-    System.out.println(" contentToTransmit length " + contentToTransmit.length);
+
+
+   // System.out.println(" contentToTransmit length " + contentToTransmit.length);
+
+
 
     anz_pakete = 1 + contentToTransmit.length/(msgsize-4);  //-4 for seq_num
-    System.out.println(" anz_pakete " + anz_pakete);
+
+    //System.out.println(" anz_pakete " + anz_pakete);
+
+
 
     ack_seq = new boolean[anz_pakete];
 
+
+
     long start = System.nanoTime();
+
     sendFile();
+
     long end = System.nanoTime();
+
     long microseconds = TimeUnit.NANOSECONDS.toMicros(end - start);
 
+
+
     double speed = 8 * (double)(contentToTransmit.length-4) / microseconds ;
+
     System.out.println("File size: "+ (contentToTransmit.length-4) +" Bytes");
+
     System.out.printf("Time elapsed: %.2f s\n", microseconds / 1000000.0);
+
     System.out.printf("Communication speed: %.2f Mbps\n",speed);
 
+
+
   }
+
+
 
   private static void sendFile() throws IOException, InterruptedException{
 
+
+
     int packets_sended = 0;
 
+
+
     int index_iterator = 0; //Index of next packet to send
+
     int attempt = 0; 
 
+
+
     //Sending Loop
+
     while(packets_sended < anz_pakete && attempt < 100){
+    	
         //Send block of packets loop
-        int count = 0; 
-        while(count < packet_block_size){
+      
+        while(true) {
             //Find next non-sended packet
+
             boolean loopEnd = false;
+
             while(ack_seq[index_iterator]){
+
                 index_iterator++;
+
                 if(index_iterator == anz_pakete){
+
                     index_iterator = 0;
+
                     loopEnd = true;
+
                     attempt++;
-                    System.out.println("Attempt: "+ attempt);
+
+                System.out.println("Attempt: "+ attempt);
+
                     break;
+
                 }
+
             }
+
             if(loopEnd)
+
                 break;
+
             sendPacket(index_iterator);
+
             if(index_iterator == anz_pakete - 1)
+
                 break;
+
             index_iterator++;
-            count++;
+
+            
+
         }
+
+
 
         //Waiting for acks
-        int delay_pro_packet = delay/anz_pakete;
+
+       
+
+
 
         byte[] buf = new byte[1000];
+
         dSocketAck.setSoTimeout((delay/1000) > 0 ? delay/1000 : 1);   // set the timeout in millisecounds.
 
+
+
         while(true){        // recieve data until timeout
+
             try {
+
                 DatagramPacket ack_packet = new DatagramPacket(buf, buf.length);
+
                 dSocketAck.receive(ack_packet);
+
                 DataInputStream ack_dis = new DataInputStream(new ByteArrayInputStream(ack_packet.getData())); 
+
                 int ack_sequenznummer = ack_dis.readInt();
-                System.out.println("=== Ack: " + ack_sequenznummer + " ===");
+
+               // System.out.println("=== Ack: " + ack_sequenznummer + " ===");
+
                 if(!ack_seq[ack_sequenznummer]){
+
                     ack_seq[ack_sequenznummer] = true;
+
                     packets_sended++;
+
                 }
+
             }
+
             catch (SocketTimeoutException e) {
+            	
+            	
                 // timeout exception.
+
                 System.out.println("Timeout reached!!! " + e);
+
                 break;
+
             }
+
         }
+
     }
 
   }
+
+  
+
+
 
   private static void sendPacket(int index) throws IOException, InterruptedException{
 
-    System.out.println( "=== Send ===" );
-    System.out.println( "seq_num: " + index);
+
+
+  //  System.out.println( "=== Send ===" );
+
+  //  System.out.println( "seq_num: " + index);
+
     byte[] sequenzNum = convertIntTo4Bytes(index); //Make sequense to 4 bytes
+
     int payloadSize = msgsize - 4;
 
+
+
     //Last packet
+
     if(index == anz_pakete-1){
+
       payloadSize = contentToTransmit.length - (msgsize - 4) * (anz_pakete - 1);
-      System.out.println( "Last packet payload: " + payloadSize);
+
+   //   System.out.println( "Last packet payload: " + payloadSize);
+
       sequenzNum[0] |= (1 << 7);
-      System.out.println( "Last packet payload: " + payloadSize);
+
+    //  System.out.println( "Last packet payload: " + payloadSize);
+
       // System.out.println(Arrays.toString(Arrays.copyOfRange(contentToTransmit, index * payloadSize, index * payloadSize + payloadSize)));
-    }
-    byte[] packetData = new byte[payloadSize + 4];
-    //Copy seq_num in packet
-    System.arraycopy(sequenzNum, 0, packetData, 0, 4); 
-    //Copy data in packet
-    System.arraycopy(contentToTransmit, index * (msgsize - 4), packetData, 4, payloadSize); 
-    if(index == anz_pakete-1){
-      System.out.println("CRC32 received:" + packetData[packetData.length - 4] + " "+ packetData[packetData.length - 3] + " " + packetData[packetData.length - 2] + " " + packetData[packetData.length - 1]);  
+
     }
 
-    DatagramPacket packet = new DatagramPacket( packetData, packetData.length, ia, port ); 
+    byte[] packetData = new byte[payloadSize + 4];
+
+    //Copy seq_num in packet
+
+    System.arraycopy(sequenzNum, 0, packetData, 0, 4); 
+
+    //Copy data in packet
+
+    System.arraycopy(contentToTransmit, index * (msgsize - 4), packetData, 4, payloadSize); 
+
+    if(index == anz_pakete-1){
+
+    //  System.out.println("CRC32 received:" + packetData[packetData.length - 4] + " "+ packetData[packetData.length - 3] + " " + packetData[packetData.length - 2] + " " + packetData[packetData.length - 1]);  
+
+    }
+
+
+
+    DatagramPacket packet = new DatagramPacket( packetData, packetData.length, ia, portRX ); 
+
+
 
     for(int i = 0; i < 10; i++) {
+
       if(i != 0)
-        System.out.print(":");
+
+      //  System.out.print(":");
+
       if(packetData[i] < 0) {
-        System.out.print(packetData[i] + 256); //Fix 2-complement +256
+
+      //  System.out.print(packetData[i] + 256); //Fix 2-complement +256
+
       } else {
-        System.out.print(packetData[i]); 
+
+       // System.out.print(packetData[i]); 
+
       }
+
     }
-    System.out.println();
+
+   // System.out.println();
+
     dSocket.send( packet );
+
   }
+
+
 
   private static byte[] readFile() throws IOException{
 
+
+
     File file = new File(fileName);
+
    
+
     FileInputStream fis = new FileInputStream(file);
+
     //create FileInputStream which obtains input bytes from a file in a file system
+
     //FileInputStream is meant for reading streams of raw bytes such as image data. For reading streams of characters, consider using FileReader.
 
+
+
     ByteArrayOutputStream bos = new ByteArrayOutputStream();
+
     byte[] buf = new byte[1024];
+
     try {
+
         for (int readNum; (readNum = fis.read(buf)) != -1;) 
+
         {
+
             //Writes to this byte array output stream
+
             bos.write(buf, 0, readNum); 
+
             
+
         }
+
     } catch (IOException ex) {
+
        
+
     }
 
+
+
     return bos.toByteArray();
+
   }
+
+
 
   private static byte[] convertIntTo4Bytes(long value){
 
+
+
     byte [] data = new byte[4];
+
     data[3] = (byte) value;
+
     data[2] = (byte) (value >>> 8);
+
     data[1] = (byte) (value >>> 16);
+
     data[0] = (byte) (value >>> 24);
+
+
 
     return data;
 
+
+
   }
+
 }
